06e96fe209344696d21efe288bf7633c412e1cea
NSAlector <103880217+NSAlector@users.noreply.github.com>
2024-12-23 15:22:24 +0300

    Add files via upload

---
+++ Lesson_1_intro/Java_practice_1_intro/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.Scanner;  
+
+class Main{
+    public static void main (String[] args) {
+    	
+    	Scanner scn = new Scanner(System.in);
+    	
+    	int val = scn.nextInt();
+    	
+    	byte b;
+    	b = 100;
+    	byte B = 12;
+    	
+    	int i = 100;
+    	long l = 20;
+    	
+    	char c = '$';
+    	
+    	boolean flag = false;
+    	
+    	
+    	float f = 2.3f;
+    	
+    	double d = 4.5;
+    	
+    	//i += l;
+    	
+    	d /= l;
+    	
+        System.out.println(val);
+        
+       // scn = new Scanner(System.in);
+        double tmp = scn.nextDouble();
+        System.out.println(tmp);
+        
+        boolean tflag = scn.nextBoolean();
+        System.out.println(tflag);
+        
+        
+    }
+}
